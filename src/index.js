1e46fec4eeef9268db8184e6b9da44af7b1f9b52
Felipe Gómez <luisfelipe_1002@hotmail.com>
2025-04-13 16:50:30 -0500

    Primer proyecto con Node.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const args = process.argv.slice(2);
+
+let min = 1;
+let max = 100;
+
+if (args.length >= 2) {
+    const parseMin = parseInt(args[0], 10);
+    const parseMax = parseInt(args[1], 10);
+
+    if (!isNaN(parseMin) && !isNaN(parseMax) && parseMin < parseMax) {
+        min = parseMin;
+        max = parseMax;
+        const randomNumber = Math.floor(Math.random()* (max-min + 1)) + min;
+        console.log(`🎲 Número aleatorio generado entre ${min} y ${max}: ${randomNumber}`);
+    } else {
+        console.log('⚠ Rango inválido. Usando valores por defecto (1-100)');
+    }
+}
+
+// const randomNumber = Math.floor(Math.random()* (max-min + 1)) + min;
+
+// console.log(`🎲 Número aleatorio generado entre ${min} y ${max}: ${randomNumber}`);
